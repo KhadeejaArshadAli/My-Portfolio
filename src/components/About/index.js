f3fd61df6ef286afe81290124c072e6445ec54cb
Khadeeja Arshad Ali <Khadijaarshad435@gmail.com>
2024-02-07 04:42:10 +0500

    done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,63 +22,63 @@ const About = () => {
 
     return (
         <>
-          <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters
-                     letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                        idx={15} />
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                            idx={15} />
 
-                </h1>
-                <p>
-                    I'm a very ambitious front-end developer looking for a role in an
-                    established IT company with the opportunity to work with the latest
-                    technologies on challenging and diverse projects.
-                </p>
-                <p align="LEFT">
-                    I'm quiet confident, naturally curious, and perpetually working on
-                    improving my chops one design problem at a time.
-                </p>
-                <p>
-                    If I need to define myself in one sentence that would be a family
-                    person, bookworm and tech-obsessed!!!
-                </p>
+                    </h1>
+                    <p>
+                        I'm a very ambitious front-end developer looking for a role in an
+                        established IT company with the opportunity to work with the latest
+                        technologies on challenging and diverse projects.
+                    </p>
+                    <p align="LEFT">
+                        I'm quiet confident, naturally curious, and perpetually working on
+                        improving my chops one design problem at a time.
+                    </p>
+                    <p>
+                        If I need to define myself in one sentence that would be a family
+                        person, bookworm and tech-obsessed!!!
+                    </p>
+                </div>
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faPhp} color="#4F5D95" />
+                        </div>
+
+                    </div>
+
+
+
+
+                </div>
             </div>
-            <div className="stage-cube-cont">
-            <div className="cubespinner">
-                <div className='face1'>
-                    <FontAwesomeIcon icon={faHtml5} color="#F06529"/>
-                </div>
-                <div className='face2'>
-                    <FontAwesomeIcon icon={faCss3} color="#28A4D9"/>
-                </div>
-                <div className='face3'>
-                    <FontAwesomeIcon icon={faReact} color="#5ED4F4"/>
-                </div>
-                <div className='face4'>
-                    <FontAwesomeIcon icon={faJsSquare} color="#EFD81D"/>
-                </div>
-                <div className='face5'>
-                    <FontAwesomeIcon icon={faGitAlt} color="#EC4D28"/>
-                </div>
-                <div className='face6'>
-                    <FontAwesomeIcon icon={faPhp} color="#4F5D95"/>
-                </div>
-                
-            </div>
+            <Loader type="pacman" />
 
-            
-
-
-        </div>
-        </div>
-        <Loader type="pacman" />
-      
 
         </>
-      
-      
+
+
     )
 }
 
